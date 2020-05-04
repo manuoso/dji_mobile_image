b200d962844e95831282e5224657c34081ce1d9e
marco montes <marrcogrova@gmail.com>
2020-05-04 14:27:08 +0200

    added multiscale face detector

--- android-videostreamdecoding/app/src/main/java/com/dji/videostreamdecoding/MainActivity.java
+++ android-videostreamdecoding/app/src/main/java/com/dji/videostreamdecoding/MainActivity.java
@@ -1,10 +1,12 @@
 package com.dji.videostreamdecoding;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
-import android.graphics.Rect;
+
 import android.graphics.YuvImage;
+import android.graphics.Rect;
 import android.media.MediaCodecInfo;
 import android.media.MediaFormat;
 import android.os.Build;
@@ -33,14 +35,20 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
 import dji.common.camera.SettingsDefinitions;
@@ -83,6 +91,16 @@ public class MainActivity extends Activity implements OnClickListener {
 
     private ImageView mScreen;
 
+    // 666 : https://github.com/opencv/opencv/tree/master/samples/android/face-detection/src/org/opencv/samples/facedetect
+    // https://stackoverflow.com/questions/33764245/android-opencv-couldnt-load-detection-based-tracker
+    private File mCascadeFile;
+    private CascadeClassifier mJavaDetector;
+    private static final Scalar FACE_RECT_COLOR     = new Scalar(0, 255, 0, 255);
+
+    private float mRelativeFaceSize;
+    private int   mAbsoluteFaceSize;
+
+
 //    static {
 //        System.loadLibrary("opencv_java");
 //    }
@@ -115,34 +133,7 @@ public class MainActivity extends Activity implements OnClickListener {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
                 {
-//                    try {
-//                        // load cascade file from application resources
-//                        InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
-//                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-//                        mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
-//                        FileOutputStream os = new FileOutputStream(mCascadeFile);
-//
-//                        byte[] buffer = new byte[4096];
-//                        int bytesRead;
-//                        while ((bytesRead = is.read(buffer)) != -1) {
-//                            os.write(buffer, 0, bytesRead);
-//                        }
-//                        is.close();
-//                        os.close();
-//
-//                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-//                        if (mJavaDetector.empty()) {
-//                            Log.e(TAG, "Failed to load cascade classifier");
-//                            mJavaDetector = null;
-//                        } else
-//                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
-//
-//                        cascadeDir.delete();
-//
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
-//                    }
+                    Boolean detectorInicializated = configureMultiscaleFaceDetector();
                 }
                     break;
                 default:
@@ -153,6 +144,62 @@ public class MainActivity extends Activity implements OnClickListener {
             }
         }
     };
+
+    private Boolean configureMultiscaleFaceDetector(){
+        // Configure face relative size
+        mRelativeFaceSize       = 0.4f;
+        mAbsoluteFaceSize       = 0;
+
+        Boolean success = false;
+        try {
+                // load cascade file from application resources
+                InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+                FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+                is.close();
+                os.close();
+
+                mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                if (mJavaDetector.empty()) {
+                    Log.e(TAG, "Failed to load cascade classifier");
+                    mJavaDetector = null;
+                } else
+                    Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+                cascadeDir.delete();
+                success = true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+        }
+
+        return success;
+    }
+
+    private MatOfRect detectFace(Mat source){
+        if (mAbsoluteFaceSize == 0) {
+            int height = source.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+
+        // Detect faces
+        MatOfRect faces = new MatOfRect();
+        if (mJavaDetector != null)
+            mJavaDetector.detectMultiScale(source, faces, 1.05, 4, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+
+        return faces;
+    }
 
     @Override
     protected void onResume() {
@@ -307,6 +354,17 @@ public class MainActivity extends Activity implements OnClickListener {
                                     cvtColor(myuv, pic, Imgproc.COLOR_YUV2BGRA_NV12);
 
                                     mMatImage = pic.clone();
+
+                                    // Detect faces in gray image
+                                    Mat image_gray = new Mat();
+                                    cvtColor(mMatImage, image_gray, Imgproc.COLOR_BGRA2GRAY);
+
+                                    MatOfRect faces = detectFace(image_gray);
+                                    org.opencv.core.Rect[] facesArray = faces.toArray();
+
+                                    // Print faces in mMatImage
+                                    for (int i = 0; i < facesArray.length; i++)
+                                        Imgproc.rectangle(mMatImage, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
 
                                     // TODO CHECK IF DISABLE IMAGE VIEW DECREASES LAG
                                     runOnUiThread(new Runnable() {
